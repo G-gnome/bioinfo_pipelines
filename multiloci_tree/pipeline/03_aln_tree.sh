025e798d092c4dd27b161ed402c98b5255da9615
Kian Kelly <kkell060@rstudio.clust.hpcc.ucr.edu>
2024-01-21 11:09:37 -0800

    initial commit

---
+++ multiloci_tree/pipeline/03_aln_tree.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -c 16 -N 1 -n 1 --mem 24gb --out logs/tree.log
+
+module load mafft
+module load fasttree
+module load clipkit
+mkdir -p aln
+
+# Alignment
+
+for genome_file in $(ls genomes/*.fasta genomes/*.fna); do
+    base=$(basename -- "$genome_file")
+    base="${base%.*}"
+
+    # ITS alignment
+    cat results/ITSx/$base/ITSx.full.fasta > aln/${base}_ITS_full_LSU.fas
+
+    # LSU alignment (excluding the first line)
+    tail -n +2 results/LSU/$base/LSU_seq.fasta >> aln/${base}_ITS_full_LSU.fas
+
+done
+
+# Combine individual alignments into a single file
+cat aln/*_ITS_full_LSU.fas > aln/ALL_ITS_full_LSU.fas
+
+# Perform MAFFT alignment on the combined file
+mafft aln/ALL_ITS_full_LSU.fas > aln/ALL_ITS_full_LSU.fasaln
+
+# Combine alignments
+clipkit aln/ALL_ITS_full_LSU.fasaln
+
+# Tree build
+FastTreeMP -nt -gamma aln/ALL_ITS_full_LSU.fasaln.clipkit > aln/Rhodotorula_ITS_LSU.fasaln.tre
